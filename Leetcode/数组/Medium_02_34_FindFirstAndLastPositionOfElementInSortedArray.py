5f1a7755f96a3c9a5f499182d096ac7f5d3fe052
Gavin <38515498+Xw-Jia@users.noreply.github.com>
2019-03-27 17:11:11 +0800

    Update Medium_02_34_FindFirstAndLastPositionOfElementInSortedArray.py

--- Leetcode/数组/Medium_02_34_FindFirstAndLastPositionOfElementInSortedArray.py
+++ Leetcode/数组/Medium_02_34_FindFirstAndLastPositionOfElementInSortedArray.py
@@ -41,4 +41,16 @@ class Solution:
         return [-1,-1]
 '''
 思路很好玩，再尝试一下
+
+step1:如果数组是空，肯定返回[-1, -1]
+step2:如果target比最大值大，比最小值小，肯定返回[-1, -1]
+step3:如果target存在于这个区间，那么：当l<=r，即区间存在时：
+      先得到，l,r,mid
+      然后如果target比mid大，则l = mid+1，r不变，mid重新计算
+      如果target比mid小， 则r =mid-1，l不变，mid重新计算
+      直到mid=target时，我们把l和r都移动到mid上，来计算一共有多少个target:
+        当l左移，r右移都存在时且与邻近相等，则移动
+        输出最后得到的区间大小[l,r]
+      
+      
 '''
